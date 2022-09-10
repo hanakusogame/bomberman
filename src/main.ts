13a81cf111f97411fff8db3efea36a0202b3017f
Masanao Baba <jjipqw@gmail.com>
2022-09-10 15:28:39 +0900

    疑似３dマップにてユニットの移動実装

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,26 @@ export function main(param: GameMainParameterObject): void {
 	const scene = new g.Scene({
 		game: g.game,
 		// このシーンで利用するアセットのIDを列挙し、シーンに通知します
-		assetIds: ["player", "shot", "se"],
+		// assetIds: ["player", "shot", "se"],
 	});
-	let time = 60; // 制限時間
-	if (param.sessionParameter.totalTimeLimit) {
-		time = param.sessionParameter.totalTimeLimit; // セッションパラメータで制限時間が指定されたらその値を使用します
-	}
+
+	// 放送者を区別する
+	let gameMasterId: string = null;
+	g.game.onJoin.addOnce((e) => {
+		gameMasterId = e.player.id;
+	});
+
 	// 市場コンテンツのランキングモードでは、g.game.vars.gameState.score の値をスコアとして扱います
 	g.game.vars.gameState = { score: 0 };
 	scene.onLoad.add(() => {
+		const title = new g.FilledRect({
+			scene: scene,
+			width: g.game.width,
+			height: g.game.height,
+			cssColor: "white",
+			parent: scene,
+		});
+
 		const font = new g.DynamicFont({
 			game: g.game,
 			fontFamily: "sans-serif",
@@ -34,20 +45,119 @@ export function main(param: GameMainParameterObject): void {
 
 		let players: { [key: string]: Player } = {};
 
+		// 参加ボタン
 		const button = new g.FilledRect({
 			scene: scene,
-			x: g.game.width - 60,
-			y: 10,
-			width: 50,
-			height: 50,
+			x: (g.game.width - 200) / 2,
+			y: (g.game.height - 80) / 2,
+			width: 200,
+			height: 80,
 			cssColor: "yellow",
-			parent: scene,
+			parent: title,
 			touchable: true,
 			local: true,
 		});
 
+		const labelButton = new g.Label({
+			scene: scene,
+			font: font,
+			y: 10,
+			width: 200,
+			text: "参加",
+			fontSize: 50,
+			textColor: "black",
+			textAlign: "center",
+			widthAutoAdjust: false,
+			parent: button,
+		});
+
+		// 開始ボタン
+		const buttonStart = new g.FilledRect({
+			scene: scene,
+			x: (g.game.width - 200) / 2 + 300,
+			y: (g.game.height - 80) / 2,
+			width: 200,
+			height: 80,
+			cssColor: "yellow",
+			parent: title,
+			touchable: true,
+			local: true,
+		});
+		buttonStart.hide();
+
+		new g.Label({
+			scene: scene,
+			font: font,
+			y: 10,
+			width: 200,
+			text: "開始",
+			fontSize: 50,
+			textColor: "black",
+			textAlign: "center",
+			widthAutoAdjust: false,
+			parent: buttonStart,
+		});
+
+		const gameBase = new g.E({
+			scene: scene,
+			parent: scene,
+		});
+		gameBase.hide();
+
+		const mapBase = new g.E({
+			scene: scene,
+			x: g.game.width / 2,
+			y: g.game.height / 2,
+			width: 0,
+			height: 0,
+			anchorX: 0,
+			anchorY: 0,
+			scaleY: 0.2,
+			parent: gameBase,
+		});
+
+		const floor = new g.FilledRect({
+			scene: scene,
+			x: 0,
+			y: 0,
+			width: 1000,
+			height: 1000,
+			anchorX: 0.5,
+			anchorY: 0.5,
+			angle: 20,
+			cssColor: "yellow",
+			parent: mapBase,
+		});
+
+		// ユニット配置用
+		const unitBase = new g.E({
+			scene: scene,
+			parent: gameBase,
+		});
+
+		floor.onUpdate.add(() => {
+			// floor.angle += 0.3;
+			// floor.modified();
+		});
+
+		// 参加人数表示
+		const labelCount = new g.Label({
+			scene: scene,
+			font: font,
+			y: -60,
+			width: 200,
+			text: "0人",
+			fontSize: 50,
+			textColor: "black",
+			textAlign: "center",
+			widthAutoAdjust: false,
+			parent: button,
+		});
+
+		// 参加
 		button.onPointDown.add((e) => {
-			button.hide();
+			button.touchable = false;
+			labelButton.text = "参加済み";
 
 			focus(); //フォーカス
 
@@ -68,6 +178,11 @@ export function main(param: GameMainParameterObject): void {
 			});
 		});
 
+		// 開始
+		buttonStart.onPointDown.add(() => {
+			g.game.raiseEvent(new g.MessageEvent({ name: "start" }));
+		});
+
 		//キーイベントを受け取る
 		scene.onMessage.add(function (msg) {
 			// 関係ないイベントは無視して抜ける
@@ -75,25 +190,30 @@ export function main(param: GameMainParameterObject): void {
 
 			const ev = msg.data;
 
-			if (ev.playerId) {
-				const player = new Player({
-					scene: scene,
-					x: (g.game.width - 50) / 2,
-					y: (g.game.height - 50) / 2,
-					width: 50,
-					height: 50,
-					cssColor: "blue",
-					parent: scene,
-				});
-				players[ev.playerId] = player;
+			if (ev.name == "start") {
+				title.hide();
+				gameBase.show();
 			}
 
+			//参加ボタンを押したとき
+			if (ev.playerId) {
+				const player = new Player(floor, unitBase);
+
+				players[ev.playerId] = player;
+
+				labelCount.text = "" + Object.keys(players).length + "人";
+				labelCount.invalidate();
+			}
+
+			//矢印キーを押したとき
 			if (ev.key) {
 				const player = players[ev.pId];
 				if (ev.isPush) {
-					if (ev.key == "a" || ev.key == "A") {
-						player.cssColor = player.cssColor == "green" ? "blue" : "green";
-					}
+					// if (ev.key == "a" || ev.key == "A") {
+					// 	player.cssColor = player.cssColor == "green" ? "blue" : "green";
+					// }
+
+					// 移動速度を設定
 					if (ev.key == "ArrowUp") {
 						player.moveY = -5;
 					}
@@ -106,7 +226,8 @@ export function main(param: GameMainParameterObject): void {
 					if (ev.key == "ArrowLeft") {
 						player.moveX = -5;
 					}
-				}else{
+				} else {
+					// 移動速度を0にする
 					if (ev.key == "ArrowUp") {
 						player.moveY = 0;
 					}
@@ -124,11 +245,15 @@ export function main(param: GameMainParameterObject): void {
 		});
 
 		scene.onUpdate.add(() => {
+			if (!buttonStart.visible()) {
+				if (gameMasterId === g.game.selfId) {
+					buttonStart.show();
+				}
+			}
+
 			for (const key in players) {
 				const player = players[key];
-				player.x += player.moveX;
-				player.y += player.moveY;
-				player.modified();
+				player.move();
 			}
 		});
 	});
@@ -136,7 +261,45 @@ export function main(param: GameMainParameterObject): void {
 }
 
 //プレイヤークラス
-class Player extends g.FilledRect {
+class Player {
 	public moveX = 0;
 	public moveY = 0;
+
+	constructor(private rectFloor: g.FilledRect, private rectMain: g.E) {}
+
+	public base = new g.FilledRect({
+		scene: g.game.scene(),
+		width: 50,
+		height: 50,
+		anchorX: 0.5,
+		anchorY: 0.5,
+		cssColor: "black",
+		parent: this.rectFloor,
+	});
+
+	public unit = new g.FilledRect({
+		scene: g.game.scene(),
+		width: 50,
+		height: 120,
+		anchorX: 0.5,
+		anchorY: 1.0,
+		cssColor: "red",
+		parent: this.rectMain,
+	});
+
+	public move() {
+		this.base.x += this.moveX;
+		this.base.y += this.moveY;
+		this.base.modified();
+
+		const gp = this.rectFloor.localToGlobal(this.base);
+		this.unit.x = gp.x;
+		this.unit.y = gp.y;
+		this.unit.modified();
+	}
 }
+
+// class Player extends g.FilledRect {
+// 	public moveX = 0;
+// 	public moveY = 0;
+// }
